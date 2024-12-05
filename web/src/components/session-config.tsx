a8f908e5829d35da41b510bd0c44fe40753c85b7
Deependra21 <deependra.joshi@ipresence.jp>
2024-12-05 17:44:40 +0900

    update

--- web/src/components/session-config.tsx
+++ web/src/components/session-config.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { TurnDetectionSelector } from "@/components/turn-detection-selector";
-import { ModalitiesSelector } from "@/components/modalities-selector";
+// import { TurnDetectionSelector } from "@/components/turn-detection-selector";
+// import { ModalitiesSelector } from "@/components/modalities-selector";
 import { VoiceSelector } from "@/components/voice-selector";
-import { TemperatureSelector } from "./temperature-selector";
+// import { TemperatureSelector } from "./temperature-selector";
 import { MaxOutputTokensSelector } from "./max-output-tokens-selector";
 import { ConfigurationFormFieldProps } from "./configuration-form";
-import { ModelSelector } from "./model-selector";
+// import { ModelSelector } from "./model-selector";
 //import { TranscriptionSelector } from "./transcription-selector";
 export function SessionConfig({ form }: ConfigurationFormFieldProps) {
   return (
